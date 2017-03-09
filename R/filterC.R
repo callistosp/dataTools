39a0497a00c1148326d86515275901cc39965d97
callistosp <callisto1002@gmail.com>
2017-03-09 10:58:34 -0600

    initial commit

---
+++ R/filterC.R
@@ -0,0 +1,16 @@
+#' Filter IGNORE=C
+#'
+#' This function filters out commented rows from a NONMEM-style
+#' dataset, and removes the comment column for plotting.
+#' @param ds longform dataset with IGNORE=C in first column
+#' @keywords
+#' @export
+#' @examples
+#' filterC()
+
+filterC <- function(ds){
+  ## filter columns with IGNORE=C
+  dsFilt <- ds[which(ds[,1] != "C"),]
+  ## return data.frame without comment column
+  return(dsFilt[,2:nrow(dsFilt)])
+}
