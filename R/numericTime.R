7cbe4336cb34b8de5f56c5ec7025f262d500dcbe
callistosp <callisto1002@gmail.com>
2017-08-22 13:51:30 -0500

    initial commit

---
+++ R/numericTime.R
@@ -0,0 +1,18 @@
+#' numericTime()
+#'
+#' This function converts clock times stored as strings
+#' into numeric hours
+#' @param vec vector containing time values of format HH:MM stored as strings
+#' @keywords
+#' @export
+#' @examples
+#' numericTime()
+
+numericTime <- function(vec){
+  sapply(strsplit(vec,":"),
+         function(x) {
+           x <- as.numeric(x)
+           round(x[1]+x[2]/60,2)
+         }
+  )
+}
