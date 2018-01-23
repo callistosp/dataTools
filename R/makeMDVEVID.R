26f320ef11208b84251b8edf2cd7c6bca0e84f70
calli055 <calli055@umn.edu>
2018-01-22 21:44:17 -0600

    don't require named SID column; allow DV == 0 or AMT == 0 in addition to NA or "."

--- R/makeMDVEVID.R
+++ R/makeMDVEVID.R
@@ -16,6 +16,9 @@
 #' @author Samuel Callisto \email{calli055@@umn.edu} makeMDVEVID()
 
 makeMDVEVID <- function(ds, DV, AMT, clearSystem=F){
+  tmpName <- names(ds)[1]
+  names(ds)[1] <- c("SID")
+
   ## initially assume all rows obs event
   ds$MDV <- 0
   ds$EVID <- 0
@@ -32,18 +35,19 @@ makeMDVEVID <- function(ds, DV, AMT, clearSystem=F){
     }
 
     ## check dosing event
-    if(!(ds[i,AMT] == "." || is.na(ds[i,AMT]))){
+    if(!(ds[i,AMT] == "." || ds[i,AMT] == 0 || is.na(ds[i,AMT]))){
       ds$MDV[i] <- 1
       ds$EVID[i] <- ifelse(clearSystem && prevDose, 4, 1)
       prevDose <- T
     }
 
     ## check missing DV
-    if((ds[i,AMT] == "." || is.na(ds[i,AMT])) && (ds[i,DV] == "." || is.na(ds[i, DV]))){
+    if((ds[i,AMT] == "." || ds[i,AMT] == 0 || is.na(ds[i,AMT])) && (ds[i,DV] == "." || ds[i,DV] == 0 || is.na(ds[i, DV]))){
       ds$MDV[i] <- 1
       ds$EVID[i] <- 2
     }
   }
 
+  names(ds)[1] <- tmpName
   return(ds)
 }
