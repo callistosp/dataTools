690ce30f53c27dac303742e3451a450434556be4
calli055 <calli055@umn.edu>
2019-01-27 20:40:45 -0600

    added new retype function and associated documentation

--- R/retype.R
+++ R/retype.R
@@ -1,6 +1,20 @@
-## TYPECAST function
-# @param df: data.frame with values to re-type
-# @param ...: unquoted list alternating variables and datatypes to convert to
+#' Retype Variable
+#'
+#' retype is a typecasting function which can be used
+#' in the tidy workflow to change the type of multiple
+#' functions within a data.frame in-line.
+#'
+#' @param df: data.frame with values to typecast
+#' @param ...: unquoted list alternating variables and datatypes to convert to
+#' @return Data.frame \code{ds} with variables listed typecast to
+#' @examples
+#' iris2 <- iris %>% select(Species, Sepal.Length) %>% retype(Species, character)
+#' str(iris2)
+#'
+#' @author Samuel Callisto \email{calli055@@umn.edu} retype()
+#' @export
+
+
 retype <- function(df, ...) {
   ## save input arguments as a vector
   vec <- as.vector(substitute(...()))
